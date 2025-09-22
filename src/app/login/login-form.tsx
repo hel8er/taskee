1712426f14fcdd061373beeca3cb2a776f7d9799
Alex Helber <helber-alx@mail.ru>
2025-09-22 23:56:06 +0300

    login page

---
+++ src/app/login/login-form.tsx
@@ -0,0 +1,44 @@
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="hover:underline font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
+            type="button"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./login.module.css";
+import LoginForm from "./login-form";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <LoginForm />
+    </div>
+  );
+}
